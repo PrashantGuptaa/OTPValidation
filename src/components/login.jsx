de3d2697bad01fefaecb583cd01cb72e2d68f789
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-12-03 00:38:56 +0530

    Bug Fix

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,30 +1,60 @@
-import React from 'react';
+import React from "react";
 
-export default (props) =>{
+export default (props) => {
+  let [num, setNum] = React.useState("");
+  let [error, setError] = React.useState("");
 
-let [num, setNum] = React.useState("");
-let [error, setError] = React.useState("");
+  let phoneNumbers = [];
 
-let phoneNumbers = []
+  const handleSubmit = () => {
+    if (num.length !== 10) {
+      console.log(num);
+      errorMessage("Digits in phone number should be equal to 10");
+      return;
+    }
+    // Validating if all digits entered are numbers.
+    if (!isValid()) {
+      errorMessage("Characters are not allowed");
+      return;
+    }
+    phoneNumbers.push(num);
+    props.history.push("/otpValidation");
+  };
 
-const handleSubmit = () => {
-if(num.length !== 10){
-  setError("Digits in phone number should be equal to 10");
-setTimeout(() => setError(""), 3000);
-return;
-}
-phoneNumbers.push(num);
-props.history.push('/otpValidation');
-}
+  const isValid = () => {
+    let temp = parseInt(num);
+    while (temp > 0) {
+      // If the input number is a character, then it'll return false
+      //since inputting characters are not allowed.
 
+      if (!isNaN(temp % 10)) {
+        return false;
+      }
 
+      temp = parseInt(temp / 10);
+    }
+    return true;
+  };
 
-  return(
-    <div className = 'container'>
+  // An Error Message will be displayed if entered
+  // number is invalid, which will be removed after 2 seconds
+  const errorMessage = (message) => {
+    setError(message);
+    setTimeout(() => setError(""), 3000);
+  };
+
+  return (
+    <div className="container">
       <h2>Enter your Mobile number to login:</h2>
-    <input onChange = {(event) => setNum(event.target.value) } type = 'Number' placeholder="Enter 10 digit Mobile number" className = 'input'/>
- <button onClick = {handleSubmit} className = 'submit' >Get OTP</button>
-   <h3 className = 'error'>{error}</h3>
+      <input
+        onChange={(event) => setNum(event.target.value)}
+        placeholder="Enter 10 digit Mobile number"
+        className="input"
+      />
+      <button onClick={handleSubmit} className="submit">
+        Get OTP
+      </button>
+      <h3 className="error">{error}</h3>
     </div>
-  )
-} 
+  );
+};
--- src/components/otpValidation.jsx
+++ src/components/otpValidation.jsx
@@ -19,29 +19,38 @@ export default (props) => {
     let sumOfDigit = 0;
     let temp = parseInt(otp);
     while (temp > 0) {
+      // If the input number is a character, then it'll return false
+      //since inputting characters are not allowed.
+      if (!isNaN(temp % 10)) {
+        return false;
+      }
+      //Calcuting sum of digits in OTP
       sumOfDigit += temp % 10;
       temp = parseInt(temp / 10);
+      console.log(sumOfDigit);
     }
     return sumOfDigit % 2 === 0;
   };
 
+  // An Error Message will be displayed if entered
+  // number is invalid, which will be removed after 2 seconds
   const errorMessage = (message) => {
     setError(message);
     setTimeout(() => setError(""), 2000);
-  }
-
-  
+  };
 
   return (
-    <div className = 'container'>
+    <div className="container">
       <h2>Enter the OTP sent to your number:</h2>
       <input
         onChange={(event) => setOtp(event.target.value)}
         placeholder="Enter 6 digit OTP"
-        className='input'
+        className="input"
       />
-      <button onClick={handleSubmit} className = 'submit'>Login</button>
-      <h3 className = 'error'>{error}</h3>
+      <button onClick={handleSubmit} className="submit">
+        Login
+      </button>
+      <h3 className="error">{error}</h3>
     </div>
   );
 };
